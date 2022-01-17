a5519a9cbd07a25e66f357a931007780d11bc1ab
alcreia <scizor.scythe@gmail.com>
2022-01-17 17:46:01 +0700

    searchbar + column

--- sources/theta-app/src/app/pages/search/search.component.ts
+++ sources/theta-app/src/app/pages/search/search.component.ts
@@ -8,25 +8,27 @@ import { SearchType } from './search-type.enum';
 
 export class SearchComponent implements OnInit {
 
-    searchPlaceholderText: string;
-    selectedType: SearchType;
+    //searchPlaceholderText: string;
+    //selectedType: SearchType;
 
     constructor() { }
 
     ngOnInit() {
-        this.selectedType = SearchType.Destination;
-        this.setSearchPlaceholderText();
+        //this.selectedType = SearchType.Destination;
+        //this.setSearchPlaceholderText();
     }
 
     onCriteriaChanged(event) {
-        this.selectedType = event.detail.value;
-        this.setSearchPlaceholderText();
+        //this.selectedType = event.detail.value;
+        //this.setSearchPlaceholderText();
     }
-
+    
+    /*
     private setSearchPlaceholderText() {
-        this.searchPlaceholderText = this.selectedType === SearchType.Destination ?
+        /*this.searchPlaceholderText = this.selectedType === SearchType.Destination ?
             `Where do you want to go?` : 'How much do you want to spend?';
     }
+    */
 
 
 }
