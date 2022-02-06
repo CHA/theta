05193d9a8ddd5186e437d778befcc2ab22c5464c
imchristofel <christofelh@gmail.com>
2022-02-06 13:41:36 +0000

    Update card layout

--- sources/theta-app/src/app/pages/search/search.component.ts
+++ sources/theta-app/src/app/pages/search/search.component.ts
@@ -1,34 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchType } from './search-type.enum';
 
 @Component({
     selector: 'tta-search',
     templateUrl: 'search.component.html'
 })
 
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-    //searchPlaceholderText: string;
-    //selectedType: SearchType;
-
+    budget = 0;
     constructor() { }
-
-    ngOnInit() {
-        //this.selectedType = SearchType.Destination;
-        //this.setSearchPlaceholderText();
-    }
-
-    onCriteriaChanged(event) {
-        //this.selectedType = event.detail.value;
-        //this.setSearchPlaceholderText();
-    }
-    
-    /*
-    private setSearchPlaceholderText() {
-        /*this.searchPlaceholderText = this.selectedType === SearchType.Destination ?
-            `Where do you want to go?` : 'How much do you want to spend?';
-    }
-    */
-
 
 }
