f8b70fad8605b1eded4e0d7a448aaf69ffbb4903
Christofel <christofelh@gmail.com>
2023-03-11 12:38:44 +0000

    Add Swagger API

--- project/itinerary-api/src/main.ts
+++ project/itinerary-api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Theta Itinerary API')
+    .setDescription('API to get travel itineraries')
+    .setVersion('1.0')
+    .addTag('itinerary')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
