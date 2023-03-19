375f372e0c9b2c04df196e3c87e0d81436bd38b2
Christofel <christofelh@gmail.com>
2023-03-19 22:57:58 +0000

    Add flex layout

--- project/theta_app/src/app/feature/feature.module.ts
+++ project/theta_app/src/app/feature/feature.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItineraryComponent } from './itinerary/itinerary.component';
 import { SharedModule } from '../shared/shared.module';
+import { ItineraryCardComponent } from './itinerary/itinerary-card/itinerary-card.component';
 
 
 
 @NgModule({
   declarations: [
-    ItineraryComponent
+    ItineraryComponent,
+    ItineraryCardComponent
   ],
   imports: [
     CommonModule,
--- project/theta_app/src/app/shared/shared.module.ts
+++ project/theta_app/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [],
@@ -13,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatGridListModule
   ],
   exports: [
     CommonModule,
@@ -20,6 +22,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatGridListModule
   ]
 })
 export class SharedModule { }
